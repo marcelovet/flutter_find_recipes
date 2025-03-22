2e37cc49654de14fc77578cdfb82259601f09725
marcelo paiva <marcelo_thelin@hotmail.com>
2025-03-22 20:19:43 -0300

    project begin

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_find_recipes
+
+A Recipe finder project to train shared_preference, networking, persistncce and state management.
---
+++ android/app/src/main/kotlin/com/example/flutter_find_recipes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_find_recipes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
