0f4ec1832c1ae2486ef063c3a45f2dc654992749
marcelo paiva <marcelo_thelin@hotmail.com>
2025-03-23 16:34:25 -0300

    repository interface

---
+++ lib/ui/recipes/recipes_detail.dart
@@ -0,0 +1,14 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lumberdash/lumberdash.dart';
+
+import '../../network/service_interface.dart';
+import '../../providers.dart';
+import '../../data/models/recipe.dart';
+import '../../network/model_response.dart';
+import '../theme/colors.dart';
+import '../widgets/common.dart';
