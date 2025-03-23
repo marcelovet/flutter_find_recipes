163c726db91271f45842f35d86c580b6d6918e41
marcelo paiva <marcelo_thelin@hotmail.com>
2025-03-22 22:46:36 -0300

    App entry with platform menu bar

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_find_recipes/utils.dart';
 import 'package:logging/logging.dart' as system_log;
 import 'package:lumberdash/lumberdash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   _setupLogging();
@@ -14,7 +15,7 @@ Future<void> main() async {
     await DesktopWindow.setWindowSize(const Size(600, 600));
     await DesktopWindow.setMinWindowSize(const Size(260, 600));
   }
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: RecipeFinder(),)); // for state management
 }
 
 void _setupLogging() {
@@ -25,22 +26,6 @@ void _setupLogging() {
   system_log.Logger.root.onRecord.listen((rec) {
       debugPrint('${rec.level.name}: ${rec.time}: ${rec.message}');
   });
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -64,10 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -85,6 +66,63 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class RecipeFinder extends StatefulWidget {
+  const RecipeFinder({super.key});
+
+  @override
+  State<RecipeFinder> createState() => _RecipeFinderState();
+}
+
+class _RecipeFinderState extends State<RecipeFinder> {
+  ThemeMode currentMode = ThemeMode.light;
+  
+  @override
+  Widget build(BuildContext context) {
+    return PlatformMenuBar(
+      menus: [
+        PlatformMenu(
+          label: 'File',
+          menus: [
+            PlatformMenuItem(
+              label: 'Dark Mode',
+              onSelected: () {
+                setState(() {
+                  currentMode = ThemeMode.dark;
+                });
+              }
+            ),
+            PlatformMenuItem(
+              label: 'Light Mode',
+              onSelected: () {
+                setState(() {
+                  currentMode = ThemeMode.light;
+                });
+              }
+            ),
+            PlatformMenuItem(
+              label: 'Quit',
+              onSelected: () {
+                setState(() {
+                  SystemNavigator.pop();
+                });
+              },
+              shortcut: const SingleActivator(LogicalKeyboardKey.keyQ, meta: true),
+            ),
+          ],
+        )
+      ],
+      child: MaterialApp(
+        title: 'Recipe Finder',
+        debugShowCheckedModeBanner: false,
+        themeMode: currentMode,
+        theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),),
+        darkTheme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),       
+      ),
     );
   }
 }
